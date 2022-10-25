21063e5766b1f04b7a236b9cf2e4ad685c678b7d
LDSamuelGuo <105694995+LDSamuelGuo@users.noreply.github.com>
2022-10-25 21:11:07 +1000

    Add files via upload

---
+++ IAB207_assignment3/website/__init__.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user
+from werkzeug.exceptions import HTTPException
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    bootstrap = Bootstrap5(app)
+    app.secret_key = "1234567890"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ticketbooking.sqlite'
+    app.config['UPLOAD_FOLDER'] = 'static/images/'
+    db.init_app(app)
+    login_manager = LoginManager()
+    from .models import User, Anonymous
+    login_manager.anonymous_user = Anonymous
+    login_manager.login_message_category = "warning"
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    from . import views, events, auth
+    app.register_blueprint(views.bp)
+    app.register_blueprint(events.eventbp)
+    app.register_blueprint(auth.authbp)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+    
+    @app.context_processor
+    def get_context():
+        # Checks if the current user is Anonymous or logged in
+        if current_user.name == 'Guest':
+            name = 'Guest'
+        else:
+            name = current_user.name
+        from website.models import Event, EventCity, EventGenre, EventStatus
+        all_events = Event.query.all()
+        # On launch, check if there are any events that are now in the past
+        # and if so, change them to Inactive
+        for event in all_events:
+            if event.date < datetime.now():
+                event.event_status=EventStatus.INACTIVE
+        current_events = Event.query.filter(Event.event_status!='INACTIVE')
+        db.session.commit()
+        dropdown_events = Event.query.group_by(Event.headliner).filter(
+        Event.event_status != 'INACTIVE').all()
+        genres = EventGenre
+        cities = EventCity
+        return(dict(events_list=all_events,artist_list=dropdown_events,genres=genres,cities=cities,username=name,current_events=current_events))
+
+    @app.errorhandler(404)
+    def not_found(e):  # error view function
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    def not_found(e):  # error view function
+        return render_template('500.html'), 500
+
+    @app.errorhandler(Exception)
+    def handle_exception(e):
+    # pass through HTTP errors
+        if isinstance(e, HTTPException):
+            return e
+    # now you're handling non-HTTP exceptions only
+        print(e)
+        return render_template('500.html'), 500
+
+    return app
---
+++ IAB207_assignment3/website/auth.py
@@ -0,0 +1,74 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .forms import LoginForm, RegisterForm
+from .models import User
+from . import db
+from flask_login import login_required, logout_user, login_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+authbp = Blueprint('auth', __name__)
+
+# Checks if the current user is Anonymous or logged in
+def is_current_user():
+    if current_user.name == 'Guest':
+        name = 'Guest'
+    else:
+        name = current_user.name
+    return name
+
+
+@authbp.route('/login', methods=['GET', 'POST'])
+def login():
+    loginForm = LoginForm()
+    if loginForm.validate_on_submit():
+        user_name = loginForm.user_name.data
+        password = loginForm.password.data
+        remember = True if request.form.get('remember') else False
+        user = User.query.filter_by(name=user_name).first()
+        # check if the user actually exists
+        # take the user-supplied password, hash it, and compare it to the hashed password in the database
+        if not user or not check_password_hash(user.password_hash, password):
+            flash('Please check your login details and try again.', 'warning')
+            # if the user doesn't exist or password is wrong, reload the page
+            return redirect(url_for('auth.login'))
+        else:
+            login_user(user, remember=remember)
+            print('Successfully logged in')
+            flash('You logged in successfully', 'success')
+            return redirect(url_for('main.index'))
+    return render_template('user.html', form=loginForm, heading='Login')
+
+
+@authbp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = RegisterForm()
+    address = []
+    if form.validate_on_submit():
+        # if this returns a user, then the email already exists in database
+        user = User.query.filter_by(emailid=form.email_id.data).first()
+        user = User.query.filter_by(name=form.user_name.data).first()
+        if user:  # if a user is found, we want to redirect back to signup page so user can try again
+            flash(
+                'Email address already exists. Login using that email or try using a different email.', 'warning')
+            return redirect(url_for('auth.register'))
+        address.append(str(form.street_no.data))
+        address.append(form.street_name.data)
+        address.append(form.state.data)
+        address.append(form.postcode.data)
+        address_string = ' '.join(str(item) for item in address)
+        user = User(name=form.user_name.data, emailid=form.email_id.data, password_hash=generate_password_hash(
+            form.password.data, method='sha256'), phone=form.phone.data, address=address_string)
+        db.session.add(user)
+        db.session.commit()
+        flash('Successfully created new user! Please login to continue.', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('user.html', form=form, heading='Register')
+
+
+@authbp.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('You have successfully logged out', 'success')
+    return redirect(url_for('main.index'))
